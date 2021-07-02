8a59c007234e9c7a1bae1e57d0f37c1792a982e8
Kinley Tshering <kxt5258@gmail.com>
2021-07-02 18:37:07 +0600

    added auth app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
+
+import Header from './components/Header';
+import Progress from './components/Progress';
+
+// lazily load code
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'con',
+});
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   return (
-    <div>
-      <h1>Hi There</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setIsSignedIn(false)}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path='/auth'>
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path='/' component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,19 +1,20 @@
-import { mount as marketingMount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
   useEffect(() => {
-    const { onParentNavigate } = marketingMount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathName }) => {
         if (history.location.pathname !== nextPathName) {
           history.push(nextPathName);
         }
       },
       initialPath: history.location.pathname,
+      onSignIn,
     });
 
     history.listen(onParentNavigate);
